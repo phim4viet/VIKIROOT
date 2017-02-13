c015af0f954612cf844440ad2ba85cb74e7342da
scoty755 <scoty755@gmail.com>
2017-02-13 19:05:04 +0900

    Use ndk-build

---
+++ Android.mk
@@ -0,0 +1,15 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+  exploit.c
+
+LOCAL_CFLAGS += -DDEBUG -D__ARM__ -Wunused
+APP_ABI := arm64-v8a
+
+LOCAL_MODULE := exploit
+LOCAL_MODULE_TAGS := optional
+LOCAL_LDFLAGS += -static
+
+include $(BUILD_EXECUTABLE)
